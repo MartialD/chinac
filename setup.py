81bd45d7388647db1af2e7c372fc39a3286f7ccf
Martial Duchamp <martial.duchamp@gmail.com>
2015-09-10 14:17:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='test_fib',
 	version='1.0',
 	author = "Martial Duchamp",
 	author_email = "martial.duchampl@gmail.com",
-	#py_modules=['module_test.fib'],
-	package_data = {'module_test' : 'fib.py' },
+	py_modules=['module_test.fib'],
+	#package_data = {'module_test' : 'fib.py' },
 	long_description = """Really long text here.""" 
 	)
 
